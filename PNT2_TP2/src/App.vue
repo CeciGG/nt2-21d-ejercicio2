2081066c1a6b6bcede8381c1b4310fbe125af52d
Cecilia Granda <ceciliagabriela.granda.contractor@gdevtools.com>
2025-11-24 19:39:13 -0300

    se cambia proyecto a vue CLI

---
+++ PNT2_TP2/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Inicio></Inicio>
+</template>
+
+<script >
+import Inicio from './components/Inicio.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Inicio
+  },
+}
+</script>
+
+<style scoped>
+</style>
